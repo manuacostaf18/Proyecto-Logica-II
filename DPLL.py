5b7e881694532ce72b575ffcddeaaa931e95ea3d
CamiloGomezV <gomezvargas38@gmail.com>
2019-11-12 15:50:32 -0500

    DPLL terminado

--- DPLL.py
+++ DPLL.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 def codificar():
     filas = [1, 2, 3, 4, 5, 6, 7, 8]
     columnas = [1, 2, 3, 4, 5, 6, 7, 8]
@@ -8,38 +9,41 @@ letras = codificar()
 
 def hay_clausula_unit(lista):
 	for n in lista:
+		#print(n)
 		if len(n) == 1:
 			return True
-		else:
-			return False
-			
+	return False
+
 
 def complemento(n):
-	 x = n[0]
-	 if	x[0] == '-':
-	 	return x[1]
-	 else:
-	 	return '-' + x		
+	x = n#[0]
+	if x[0] == '-':
+		return x[1]
+	else:
+		return '-' + x
 
-			
+
 def unit_propagate(S, I):
+	#print("Haciendo unit propagate")
+	#print(S, I)
 	c_vacio = []
-	while(c_vacio not in S and hay_clausula_unit(S)):
+	aux = hay_clausula_unit(S)
+	#print(aux)
+	while(c_vacio not in S and aux):
 		for n in S:
 			if len(n) == 1:
 				l = n[0]
-		for y in S:
-			if l in y:
-				S.remove(y)
+		S = [y for y in S if l not in y]
 		for w in S:
 			if complemento(l) in w:
 				w.remove(complemento(l))
 		if l[0] == '-':
-			I[l] = 0
+			I[l[1]] = 0
 		else:
 			I[l] = 1
+		aux = hay_clausula_unit(S)
 	return S, I
-			
+
 
 def DPLL(S, I):
 	S, I = unit_propagate(S, I)
@@ -48,66 +52,78 @@ def DPLL(S, I):
 		return "Insatisfacible", {}
 	elif len(S) == 0:
 		return "Satisfacible", I
+	l = ""
 	for n in S:
 		for x in n:
 			if x not in I.keys():
-				l = x	
-	Sp = S
-	for w in S:
-		if l in w:
-			Sp.remove(w)
-	for m in S:
-		if complemento(l) in m:
-			m.remove(complemento(l))		
-	Ip = I
+				l = x
+	lBarra = complemento(l)
+	if l == "":
+		print("Oh oh, problemas...")
+		return None
+	Sp = deepcopy(S)
+	Sp = [n for n in Sp if l not in n]
+	for m in Sp:
+		if lBarra in m:
+			m.remove(lBarra)	
+	Ip = deepcopy(I)
 	if l[0] == '-':
-		Ip[l] = 0
+		Ip[l[1]] = 0
 	else:
 		Ip[l] = 1
-	if DPLL(Sp, Ip) == ("Satisfacible", Ip):
-		return "Satisfacible", Ip
+
+	S1, I1 = DPLL(Sp, Ip)
+	if S1 == "Satisfacible":
+		return S1, I1
 	else:
-		Spp = S
+		Spp = deepcopy(S)
 		for a in Spp:
 			if complemento(l) in a:
 				Spp.remove(a)
 		for b in Spp:
 			if l in b:
 				b.remove(l)
-		Ipp = I
+		Ipp = deepcopy(I)
 		if l[0] == '-':
-			Ipp[l] = 0
+			Ipp[l[1]] = 0
 		else:
 			Ipp[l] = 1
 		return DPLL(Spp, Ipp)
-				
-		
-	
-	
-	
-		
+
+
+
+
+
+
 S1 = [['p'], ['-p', 'q'], ['-q', 'r', 's'], ['u', '-s', 'r'], ['r', 't'], ['p', 's', '-t'], ['-r', 'u']]
 prueba1 = unit_propagate(S1, {})
-print(prueba1)
+print("prueba1: ", prueba1)
 
 S2 = [['p'], ['-p', 'q', '-r'], ['q']]
 prueba2 = DPLL(S2, {})
-print(prueba2)
+print("prueba2: ", prueba2)
 
 S3 = [['p'], ['-p', 'q'], ['-q', 'r', 's']]
 prueba3 = DPLL(S3, {})
-print(prueba3)
+print("prueba3: ", prueba3)
 
-S4 = [['p', '-q', 'r'], ['-p', 'q', '-r'], ['-p', '-q', 'r'], ['-p', '-q', '-r']]
+S4 = [['p', 'q', 'r'], ['-p', '-q', '-r'], ['-p', 'q', 'r'], ['-q', 'r'], ['q', '-r']]
 prueba4 = DPLL(S4, {})
-print(prueba4)
+print("prueba4: ", prueba4)
 
+S5 = [['p', 'q', 'r', '-s'], ['p', 't', 's'], ['-p', '-q'], ['p', 'r', '-q', '-s']]
+prueba5 = DPLL(S5, {})
+print("prueba5: ", prueba5)
 
+S6 = [['p', 'q', '-r'], ['r', 's', 't'], ['t'], ['p', 's'], ['q', '-p']]
+prueba6 = DPLL(S6, {})
+print("prueba6: ", prueba6)
 
+S7 = [['p', '-q'], ['-p', '-q'], ['q', 'r'], ['-q', '-r'], ['-p', '-r'], ['p', '-r']]
+prueba7 = DPLL(S7, {})
+print("prueba7: ", prueba7)
 
-
-
-
-
-
+S8 = [['r', 'p', 's'], ['-r', '-p', '-s'], ['-r', 'p', 's'], ['p', '-s']]
+prueba8 = DPLL(S8, {})
+print("prueba8: ", prueba8)
 
