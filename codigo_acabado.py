089ef52b1a2c3439d3bd5446a6f065f6dd37325f
manuacostaf18 <55296684+manuacostaf18@users.noreply.github.com>
2019-11-23 20:49:35 -0500

    Update codigo_acabado.py

--- codigo_acabado.py
+++ codigo_acabado.py
@@ -2,8 +2,7 @@ from copy import deepcopy
 import sys
 sys.setrecursionlimit(10000)
 
-
-def hay_clausula_unit(lista):
+def hay_clausula_unit(lista):#Retorna true si hay una cláusula unitaria.
 	for n in lista:
 		#print(n)
 		if len(n) == 1:
@@ -11,7 +10,7 @@ def hay_clausula_unit(lista):
 	return False
 
 
-def complemento(n):
+def complemento(n):#Retorna el complemento de la cláusula unitaria ingresada. 
 	x = n#[0]
 	if x[0] == '-':
 		return x[1]
@@ -19,7 +18,7 @@ def complemento(n):
 		return '-' + x
 
 
-def unit_propagate(S, I):
+def unit_propagate(S, I):#Función que realiza la propagación de la unidad.
 	c_vacio = []
 	aux = hay_clausula_unit(S)
 	while(c_vacio not in S and aux):
@@ -38,7 +37,7 @@ def unit_propagate(S, I):
 	return S, I
 
 
-def DPLL(S, I):
+def DPLL(S, I):#Retorna si es satisfacible o no, y, en caso de que lo sea, la interpretación con la cual se satisface.
 	S, I = unit_propagate(S, I)
 	c_vacio = []
 	if c_vacio in S:
@@ -84,7 +83,7 @@ def DPLL(S, I):
 		return DPLL(Spp, Ipp)
 
 
-def enFNC(A):
+def enFNC(A):#Convierte una fórmula a su forma normal conjuntiva.
     assert(len(A)==4 or len(A)==7), u"Fórmula incorrecta!"
     B = ''
     p = A[0]
@@ -117,7 +116,7 @@ def enFNC(A):
     return B
 
 
-def Tseitin(A, letrasProposicionalesA):
+def Tseitin(A, letrasProposicionalesA):#Realiza el procedimiento de Tseitin.
     letrasProposicionalesB = [chr(x) for x in range(256, 900000)]
     assert(not bool(set(letrasProposicionalesA) & set(letrasProposicionalesB))), u"¡Hay letras proposicionales en común!"
     L =[]
@@ -162,7 +161,7 @@ def Tseitin(A, letrasProposicionalesA):
     return "OK"
 
 
-def Clausula(C):
+def Clausula(C):#Convierte una fórmula en una cláusula.
     L = []
     while len(C) > 0:
         s = C[0]
@@ -179,7 +178,7 @@ def Clausula(C):
     return "OK"
 
 
-def formaClausal(A):
+def formaClausal(A):#Retorna la forma clausal de una fórmula.
     L = []
     i = 0
     while len(A) > 0:
@@ -197,14 +196,14 @@ def formaClausal(A):
     return "OK"
 
 
-class Tree():
+class Tree():#Clase árbol.
     def __init__(self,label,iz,der):
         self.label = label
         self.left = iz
         self.right = der
 
 
-def string2Tree(A):
+def string2Tree(A):#Convierte una cadena de string en un objeto árbol.
     conectivos = ["*","+",">"]
     stack = []
     for c in A:
@@ -222,7 +221,7 @@ def string2Tree(A):
     return stack[-1]
 
 
-def Inorder(arbol):
+def Inorder(arbol):#Retorna la forma 'inorder' de un árbol.
 	conectivosBinarios = ["*","+",">"]
 	if arbol.label in letras:
 		return arbol.label
@@ -242,7 +241,7 @@ def diccionario(dic): #retorna un diccionario con los valores de las 64 letras p
     return d
 
 
-def balanced(f):
+def balanced(f):#retorna true si la fórmula tiene paréntesis balanceados.
 	s = []
 	for i in f:
 		if i == '(':
@@ -272,173 +271,124 @@ def lista_true(dic): #retorna una lista con las letras que tienen valor 1.
         d.append(n)
     return d
             
-
+#Lista de letras proposicionales.
 letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"]
 
-regla_a1 = 'l-k-j-h-g-f-e-d-c-b-3-U-N-F-x-p-i-a*****************'
-regla_a2 = 'o-ñ-n-m-l-k-j-s-r-q-d-c-b-3-U-N-F-x-p-a-i********************'
-regla_a3 = 'A-z-y-l-k-j-w-v-u-t-s-r-q-3-U-N-F-x-i-a-p********************'
-regla_a4 = 'I-H-G-s-r-q-E-D-C-B-A-z-y-3-U-N-F-p-i-a-x********************'
-regla_a5 = 'P-O-Ñ-A-z-y-M-L-K-J-I-H-G-3-U-N-x-p-i-a-F********************'
-regla_a6 = 'X-W-V-I-H-G-T-S-R-Q-P-O-Ñ-3-U-F-x-p-i-a-N********************'
-regla_a7 = '6-5-4-P-O-Ñ-2-1-Z-Y-X-W-V-3-N-F-x-p-i-a-U********************'
-regla_a8 = 'X-W-V-0-9-8-7-6-5-4-U-N-F-x-p-i-a-3*****************'
+#Reglas
+
+#Reglas casilla a:
+regla_a1 = '3-U-N-F-x-p-l-k-j-i-h-g-f-e-d-c-b-a*****************'
+regla_a2 = '3-U-N-F-x-s-r-q-p-o-ñ-n-m-l-k-j-d-c-b-a-i********************'
+regla_a3 = '3-U-N-F-A-z-y-x-w-v-u-t-s-r-q-l-k-j-i-a-p********************'
+regla_a4 = '3-U-N-I-H-G-F-E-D-C-B-A-z-y-s-r-q-p-i-a-x********************'
+regla_a5 = '3-U-P-O-Ñ-N-M-L-K-J-I-H-G-A-z-y-x-p-i-a-F********************'
+regla_a6 = '3-X-W-V-U-T-S-R-Q-P-O-Ñ-I-H-G-F-x-p-i-a-N********************'
+regla_a7 = '6-5-4-3-2-1-Z-Y-X-W-V-P-O-Ñ-N-F-x-p-i-a-U********************'
+regla_a8 = '0-9-8-7-6-5-4-X-W-V-U-N-F-x-p-i-a-3*****************'
 regla_a = regla_a8 + regla_a7 + regla_a6 + regla_a5 + regla_a4 + regla_a3 + regla_a2 + regla_a1 + '+++++++'
-ade = string2Tree(regla_a)
-#print("revisando letra A")
-#print(Inorder(ade))
 
-regla_b1 = 'n-m-k-i-h-g-f-e-d-c-a-4-V-Ñ-G-y-q-j-b******************'
-regla_b2 = 'u-t-r-p-f-e-c-a-o-ñ-n-m-l-k-i-4-V-Ñ-G-y-q-b-j**********************'
-regla_b3 = 'C-B-z-x-n-m-k-i-w-v-u-t-s-r-p-4-V-Ñ-G-y-j-b-q**********************'
-regla_b4 = 'K-J-H-F-u-t-r-p-E-D-C-B-A-z-x-4-V-Ñ-G-q-j-b-y**********************'
-regla_b5 = 'R-Q-O-N-C-B-y-x-M-L-K-J-I-H-F-4-V-Ñ-y-q-j-b-G**********************'
-regla_b6 = 'Z-Y-W-U-K-J-H-F-T-S-R-Q-P-O-N-4-V-G-y-q-j-b-Ñ**********************'
-regla_b7 = '8-7-5-3-R-Q-O-N-2-1-Z-Y-X-W-U-4-Ñ-G-y-q-j-b-V**********************'
-regla_b8 = 'Z-Y-W-U-0-9-8-7-6-5-3-b-V-Ñ-G-y-q-j-4******************'
+#Reglas casilla b:
+regla_b1 = '4-V-Ñ-G-y-q-n-m-k-j-i-h-g-f-e-d-c-a-b******************'
+regla_b2 = '4-V-Ñ-G-y-u-t-r-q-p-o-ñ-n-m-l-k-i-f-e-c-b-a-j**********************'
+regla_b3 = '4-V-Ñ-G-C-B-z-y-x-w-v-u-t-s-r-p-n-m-k-j-i-b-q**********************'
+regla_b4 = '4-V-Ñ-K-J-H-G-F-E-D-C-B-A-z-x-u-t-r-q-p-j-b-y**********************'
+regla_b5 = '4-V-R-Q-O-Ñ-N-M-L-K-J-I-H-F-C-B-z-y-x-q-j-b-G**********************'
+regla_b6 = '4-Z-Y-W-V-U-T-S-R-Q-P-O-N-K-J-H-G-F-y-q-j-b-Ñ**********************'
+regla_b7 = '8-7-5-4-3-2-1-Z-Y-X-W-U-R-Q-O-Ñ-N-G-y-q-j-b-V**********************'
+regla_b8 = '0-9-8-7-6-5-3-Z-Y-W-V-U-Ñ-G-y-q-j-b-4******************'
 regla_b = regla_b8 + regla_b7 + regla_b6 + regla_b5 + regla_b4 + regla_b3 + regla_b2 + regla_b1 + '+++++++'
-ad = string2Tree(regla_b)
-#print("revisando letra B")
-#print(Inorder(ad))
 
-regla_c1 = 'ñ-n-m-l-j-i-h-g-f-e-d-b-a-5-W-O-H-z-r-k-c********************'
-regla_c2 = 'o-ñ-n-m-l-j-i-v-u-t-s-q-p-g-f-e-d-b-a-5-W-O-H-z-r-c-k**************************'
-regla_c3 = 'D-C-B-A-y-x-w-v-u-t-s-q-p-ñ-n-m-l-j-i-5-W-O-H-z-k-c-r**************************'
-regla_c4 = 'L-K-J-I-G-F-E-D-C-B-A-y-x-v-u-t-s-q-p-5-W-O-H-r-k-c-z**************************'
-regla_c5 = 'S-R-Q-P-Ñ-N-M-L-K-J-I-G-F-D-C-B-A-y-x-5-W-O-z-r-k-c-H**************************'
-regla_c6 = '1-Z-Y-X-V-U-T-S-R-Q-P-Ñ-N-L-K-J-I-G-F-5-W-H-z-r-k-c-O**************************'
-regla_c7 = '9-8-7-6-4-3-2-1-Z-Y-X-V-U-S-R-Q-P-Ñ-N-5-O-H-z-r-k-c-W**************************'
-regla_c8 = '0-9-8-7-6-4-3-1-Z-Y-X-V-U-c-W-O-H-z-r-k-5********************'
+#Reglas casilla c:
+regla_c1 = '5-W-O-H-z-r-ñ-n-m-l-k-j-i-h-g-f-e-d-b-a-c********************'
+regla_c2 = '5-W-O-H-z-v-u-t-s-r-q-p-o-ñ-n-m-l-j-i-g-f-e-d-c-b-a-k**************************'
+regla_c3 = '5-W-O-H-D-C-B-A-z-y-x-w-v-u-r-s-q-p-ñ-n-m-l-k-j-i-c-r**************************'
+regla_c4 = '5-W-O-L-K-J-I-H-G-F-E-D-C-B-A-y-x-v-u-t-s-r-q-p-k-c-z**************************'
+regla_c5 = '5-W-S-R-Q-P-O-Ñ-N-M-L-K-J-I-G-F-D-C-B-A-z-y-x-r-k-c-H**************************'
+regla_c6 = '5-1-Z-Y-X-W-V-U-T-S-R-Q-P-Ñ-N-L-K-J-I-H-G-F-z-r-k-c-O**************************'
+regla_c7 = '9-8-7-6-5-4-3-2-1-Z-Y-X-V-U-S-R-Q-P-O-Ñ-N-H-z-r-k-c-W**************************'
+regla_c8 = '0-9-8-7-6-4-3-1-Z-Y-X-W-V-U-O-H-z-r-k-c-5********************'
 regla_c = regla_c8 + regla_c7 + regla_c6 + regla_c5 + regla_c4 + regla_c3 + regla_c2 + regla_c1 + '+++++++'
 
-
-regla_d1 = 'ñ-n-k-i-h-g-f-e-c-b-a-6-X-P-I-A-s-l-d******************'
-regla_d2 = 'v-u-r-p-o-ñ-n-m-k-j-i-g-f-c-a-6-X-P-I-A-s-d-l**********************'
-regla_d3 = 'D-C-z-x-w-v-u-t-r-q-p-ñ-n-k-i-6-X-P-I-A-l-d-s**********************'
-regla_d4 = 'L-K-H-F-E-D-C-B-z-y-x-v-u-r-p-6-X-P-I-l-s-d-A**********************'
-regla_d5 = 'S-R-O-N-M-L-K-J-H-G-F-D-C-z-x-6-X-P-A-l-s-d-I**********************'
-regla_d6 = '1-Z-W-U-T-S-R-Q-O-Ñ-N-L-K-H-F-6-X-I-A-l-s-d-P**********************'
-regla_d7 = '9-8-5-3-2-1-Z-Y-W-V-U-S-R-O-N-6-P-I-A-l-s-d-X**********************'
-regla_d8 = '0-9-8-7-5-4-3-1-Z-W-U-d-X-P-I-A-s-l-6******************'
+#Reglas casilla d:
+regla_d1 = '6-X-P-I-A-s-ñ-n-l-k-i-h-g-f-e-c-b-a-d******************'
+regla_d2 = '6-X-P-I-A-v-u-s-r-p-o-ñ-n-m-k-j-i-g-f-d-c-a-l**********************'
+regla_d3 = '6-X-P-I-D-C-A-z-x-w-v-u-t-r-q-p-ñ-n-l-k-i-d-s**********************'
+regla_d4 = '6-X-P-L-K-I-H-F-E-D-C-B-z-y-x-v-u-s-r-p-l-d-A**********************'
+regla_d5 = '6-X-S-R-P-O-N-M-L-K-J-H-G-F-D-C-A-z-x-s-l-d-I**********************'
+regla_d6 = '6-1-Z-X-W-U-T-S-R-Q-O-Ñ-N-L-K-I-H-F-A-s-l-d-P**********************'
+regla_d7 = '9-8-6-5-3-2-1-Z-Y-W-V-U-S-R-P-O-N-I-A-s-l-d-X**********************'
+regla_d8 = '0-9-8-7-5-4-3-1-Z-X-W-U-P-I-A-s-l-d-6******************'
 regla_d = regla_d8 + regla_d7 + regla_d6 + regla_d5 + regla_d4 + regla_d3 + regla_d2 + regla_d1 + '+++++++'
-ade = string2Tree(regla_d)
-#print("revisando letra D")
-#print(Inorder(ade))
 
-regla_e1 = 'o-n-k-j-h-g-f-d-c-b-a-7-Y-Q-J-B-t-m-e******************'
-regla_e2 = 'v-u-r-q-h-f-c-b-o-ñ-n-l-k-j-i-7-Y-Q-J-B-t-e-m**********************'
-regla_e3 = 'E-C-z-y-w-v-u-s-r-q-p-o-n-k-j-7-Y-Q-J-B-m-e-t**********************'
-regla_e4 = 'M-K-H-G-E-D-C-A-z-y-x-w-u-r-q-7-Y-Q-J-t-m-e-B**********************'
-regla_e5 = 'T-R-O-Ñ-M-L-K-I-H-G-F-E-C-z-y-7-Y-Q-B-t-m-e-J**********************'
-regla_e6 = '2-Z-W-V-T-S-R-P-O-Ñ-N-M-K-H-G-7-Y-J-B-t-m-e-Q**********************'
-regla_e7 = '0-8-5-4-2-1-Z-X-W-V-U-T-R-O-Ñ-7-Q-J-B-t-m-e-Y**********************'
-regla_e8 = '0-9-8-6-5-4-3-2-Z-W-V-e-Y-Q-J-B-t-m-7******************'
+#Reglas casilla e:
+regla_e1 = '7-Y-Q-J-B-t-o-n-m-k-j-h-g-f-d-c-b-a-e******************'
+regla_e2 = '7-Y-Q-J-B-w-u-t-r-q-o-ñ-n-l-k-j-i-h-f-r-c-b-m**********************'
+regla_e3 = '7-Y-Q-J-E-C-B-z-y-w-v-u-s-r-q-p-o-n-m-k-j-e-t**********************'
+regla_e4 = '7-Y-Q-M-K-J-H-G-E-D-C-A-z-y-x-w-u-t-r-q-m-e-B**********************'
+regla_e5 = '7-Y-T-R-Q-O-Ñ-M-L-K-I-H-G-F-E-C-B-z-y-t-m-e-J**********************'
+regla_e6 = '7-2-Z-Y-W-V-T-S-R-P-O-Ñ-N-M-K-J-H-G-B-t-m-e-Q**********************'
+regla_e7 = '0-8-7-5-4-2-1-Z-X-W-V-U-T-R-Q-O-Ñ-J-B-t-m-e-Y**********************'
+regla_e8 = '0-9-8-6-5-4-3-2-Z-Y-W-V-Q-J-B-t-m-e-7******************'
 regla_e = regla_e8 + regla_e7 + regla_e6 + regla_e5 + regla_e4 + regla_e3 + regla_e2 + regla_e1 + '+++++++'
-ade = string2Tree(regla_e)
-#print("revisando letra E")
-#print(Inorder(ade))
 
-regla_f1 = 'o-ñ-m-l-k-j-h-g-e-d-c-b-a-8-Z-R-K-C-u-n-f********************'
-regla_f2 = 'w-v-t-s-r-q-o-ñ-m-l-k-j-i-h-g-e-d-c-b-8-Z-R-K-C-u-f-n**************************'
-regla_f3 = 'E-D-B-A-z-y-w-v-t-s-r-q-p-o-ñ-m-l-k-j-8-Z-R-K-C-n-f-u**************************'
-regla_f4 = 'M-L-J-I-H-G-E-D-B-A-z-y-x-w-v-t-s-r-q-8-Z-R-K-u-n-f-C**************************'
-regla_f5 = 'T-S-Q-P-O-Ñ-M-L-J-I-H-G-F-E-D-B-A-z-y-8-Z-R-C-u-n-f-K**************************'
-regla_f6 = '2-1-Y-X-W-V-T-S-Q-P-O-Ñ-N-M-L-J-I-H-G-8-Z-K-C-u-n-f-R**************************'
-regla_f7 = '0-9-7-6-5-4-2-1-Y-X-W-V-U-T-S-Q-P-O-Ñ-8-R-K-C-u-n-f-Z**************************'
-regla_f8 = '0-9-7-6-5-4-3-2-1-Y-X-W-V-f-Z-R-K-C-u-n-8********************'
+#Reglas casilla f:
+regla_f1 = '8-Z-R-K-C-u-o-ñ-n-m-l-k-j-h-g-e-d-c-b-a-f********************'
+regla_f2 = '8-Z-R-K-C-w-v-u-t-s-r-q-o-ñ-m-l-k-j-i-h-g-f-e-d-c-b-n**************************'
+regla_f3 = '8-Z-R-K-E-D-C-B-A-z-y-w-v-t-s-r-q-p-o-ñ-n-m-l-k-j-f-u**************************'
+regla_f4 = '8-Z-R-M-L-K-J-I-H-G-E-D-B-A-z-y-x-w-v-u-t-s-r-q-n-f-C**************************'
+regla_f5 = '8-Z-T-S-R-Q-P-O-Ñ-M-L-J-I-H-G-F-E-D-C-B-A-z-y-u-n-f-K**************************'
+regla_f6 = '8-2-1-Z-Y-X-W-V-T-S-Q-P-O-Ñ-N-M-L-K-J-I-H-G-C-u-n-f-R**************************'
+regla_f7 = '0-9-8-7-6-5-4-2-1-Y-X-W-V-U-T-S-R-Q-P-O-Ñ-K-C-u-n-f-Z**************************'
+regla_f8 = '0-9-7-6-5-4-3-2-1-Z-Y-X-W-V-R-K-C-u-n-f-8********************'
 regla_f = regla_f8 + regla_f7 + regla_f6 + regla_f5 + regla_f4 + regla_f3 + regla_f2 + regla_f1 + '+++++++'
 
-
-regla_g1 = 'o-n-l-k-h-f-e-d-c-b-a-9-1-S-L-D-v-ñ-g******************'
-regla_g2 = 'w-u-s-r-o-n-m-l-k-j-i-h-f-d-c-9-1-S-L-D-v-g-ñ**********************'
-regla_g3 = 'E-C-A-z-w-u-t-s-r-q-p-o-n-l-k-9-1-S-L-D-ñ-g-v**********************'
-regla_g4 = 'M-K-I-H-E-C-B-A-z-y-x-w-u-s-r-9-1-S-L-v-ñ-g-D**********************'
-regla_g5 = 'T-R-P-O-M-K-J-I-H-G-F-E-C-A-z-9-1-S-D-v-ñ-g-L**********************'
-regla_g6 = '2-Z-X-W-T-R-Q-P-O-Ñ-N-M-K-I-H-9-1-L-D-v-ñ-g-S**********************'
-regla_g7 = '0-8-6-5-2-Z-Y-X-W-V-U-T-R-P-O-9-S-L-D-v-ñ-g-1**********************'
-regla_g8 = '0-8-7-6-5-4-3-2-Z-X-W-g-1-S-L-D-v-ñ-9******************'
+#Reglas casilla g:
+regla_g1 = '9-1-S-L-D-v-o-ñ-n-l-k-h-f-e-d-c-b-a-g******************'
+regla_g2 = '9-1-S-L-D-w-v-u-s-r-o-n-m-l-k-j-i-h-g-f-d-c-ñ**********************'
+regla_g3 = '9-1-S-L-E-D-C-A-z-w-u-t-s-r-q-p-o-ñ-n-l-k-g-v**********************'
+regla_g4 = '9-1-S-M-L-K-I-H-E-C-B-A-z-y-x-w-v-u-s-r-ñ-g-D**********************'
+regla_g5 = '9-1-T-S-R-P-O-M-K-J-I-H-G-F-E-D-C-A-z-v-ñ-g-L**********************'
+regla_g6 = '9-2-1-Z-X-W-T-R-Q-P-O-Ñ-N-M-L-K-I-H-D-v-ñ-g-S**********************'
+regla_g7 = '0-9-8-6-5-2-Z-Y-X-W-V-U-T-S-R-P-O-L-D-v-ñ-g-1**********************'
+regla_g8 = '0-8-7-6-5-4-3-2-1-Z-X-W-S-L-D-v-ñ-g-9******************'
 regla_g = regla_g8 + regla_g7 + regla_g6 + regla_g5 + regla_g4 + regla_g3 + regla_g2 + regla_g1 + '+++++++'
-ade = string2Tree(regla_g)
-print("revisando letra G")
-print(Inorder(ade))
 
-regla_h1 = 'ñ-n-m-g-f-e-d-c-b-a-0-2-T-M-E-w-o-h*****************'
-regla_h2 = 'v-u-t-g-f-e-ñ-n-m-l-k-j-i-0-2-T-M-E-w-h-o********************'
-regla_h3 = 'D-C-B-v-u-t-s-r-q-p-ñ-n-m-0-2-T-M-E-o-h-w********************'
-regla_h4 = 'L-K-J-D-C-B-A-z-y-x-v-u-t-0-2-T-M-w-o-h-E********************'
-regla_h5 = 'S-R-Q-L-K-J-I-H-G-F-D-C-B-0-2-T-E-w-o-h-M********************'
-regla_h6 = '1-Z-Y-S-R-Q-P-O-Ñ-N-L-K-J-0-2-M-E-w-o-h-T********************'
-regla_h7 = '9-8-7-1-Z-Y-X-W-V-U-S-R-Q-0-T-M-E-w-o-h-2********************'
-regla_h8 = '9-8-7-6-5-4-3-1-Z-Y-h-2-T-M-E-w-o-0*****************'
+#Reglas casilla h:
+regla_h1 = '0-2-T-M-E-w-o-ñ-n-m-g-f-e-d-c-b-a-h*****************'
+regla_h2 = '0-2-T-M-E-w-v-u-t-ñ-n-m-l-k-j-i-h-g-f-e-o********************'
+regla_h3 = '0-2-T-M-E-D-C-B-v-u-t-s-r-q-p-o-ñ-n-m-h-w********************'
+regla_h4 = '0-2-T-M-L-K-J-D-C-B-A-z-y-x-w-v-u-t-o-h-E********************'
+regla_h5 = '0-2-T-S-R-Q-L-K-J-I-H-G-F-E-D-C-B-w-o-h-M********************'
+regla_h6 = '0-2-1-Z-Y-S-R-Q-P-O-Ñ-N-M-L-K-J-E-w-o-h-T********************'
+regla_h7 = '0-9-8-7-1-Z-Y-X-W-V-U-T-S-R-Q-M-E-w-o-h-2********************'
+regla_h8 = '9-8-7-6-5-4-3-2-1-Z-Y-T-M-E-w-o-h-0*****************'
+regla_hh = 'T-M-E-w-***'
 regla_h = regla_h8 + regla_h7 + regla_h6 + regla_h5 + regla_h4 + regla_h3 + regla_h2 + regla_h1 + '+++++++'
-ade = string2Tree(regla_h)
-#print("revisando letra H")
-#print(Inorder(ade))
 
+#Pruebas concatenación de reglas:
+prueba1 = regla_d + regla_c + regla_b + regla_a + '***'
+prueba2 = regla_h + regla_g + regla_f + regla_e + '***'
 
+#Reglas finales:
 regla = regla_h + regla_g + regla_f + regla_e + regla_d + regla_c + regla_b + regla_a + '*******'
+regla_final = regla + regla_hh + '*'
 
-
-#prueba1 = regla_a + regla_b + regla_c + '**'
-#prueba2 = regla_a + regla_h + '*'
-#prueba3 = regla_a + regla_b + regla_g + regla_h + '***'
-#prueba4 = regla_a + regla_b + regla_d + '**'
-prueba1 = regla_a + regla_b + '*' 
-prueba2 = regla_c + regla_d + '*'
-prueba3 = regla_e + regla_f + '*'
-prueba4 = regla_h + regla_g + '*' 
-#check //
-#regla_h con regla_f = 34
-#regla_a con regla_c = 34
-#-------------
-#regla_a con regla_b = 36
-#regla_a con regla_d = 37 -problema
-#regla_h con regla_e = 36
-#regla_h con regla_g = 31 -problemas serios.
-#a = 47
-#b = 46 
-#c = 44
-#d = 46 
-#e = 46 
-#f = 44
-#g = 46 
-#h = 47
-
-
-rprueba = string2Tree(regla)
+rprueba = string2Tree(regla_final)
 r = Inorder(rprueba)
-print("------------------------------------")
-print("inorder de las reglas")
 print(r)
-#print("------------------------------------")
 prueba_tseitin = Tseitin(r, letras)
 prueba_clausal = formaClausal(prueba_tseitin)
-#print("formula clausal de las reglas")
 #print(prueba_clausal)
 prueba_dpll = DPLL(prueba_clausal, {})
-#print("------------------------------------")
-#print('DPLL:', prueba_dpll)
-#print("------------------------------------")
+print('DPLL:', prueba_dpll)
 dic_prueba = diccionario(prueba_dpll[1])
-#print(dic_prueba)
-#print('Valores de las letras:', dic_prueba)
+print('')
+print('Valores de las letras:', dic_prueba)
 #print(len(dic_prueba))
 dic_final = diccionario_true(dic_prueba)
-print('------------------------------------')
-print('Letras con True:', dic_final)
-print("longitud de el diccionario final")
-print(len(dic_final))
-
-
-
-
-
-
 print('')
 print('Diccionario letras con True:', dic_final)
-#print(len(dic_final))
+print(len(dic_final))
 lista = lista_true(dic_final)
 print('')
 print('Lista letras con true:', lista)
-
-
